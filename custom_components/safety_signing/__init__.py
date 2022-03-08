4c39f5cd6e6244af21710bb1298d0e81376d2329
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-08 14:36:12 +0700

    -

--- custom_components/safety_signing/__init__.py
+++ custom_components/safety_signing/__init__.py
@@ -18,8 +18,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # with your actual devices.
     try:
         hass.data.setdefault(DOMAIN, {})[entry.entry_id]
+
+        input_config = json.loads(entry.data["config"])
+        input_config["token_serial"]
+        input_config["serial_number"]
+        input_config["pin"]
+        input_config["access_token"]
+        input_config["app"]
     except:
-        hass.data.setdefault(DOMAIN, {})[entry.entry_id] = token.Token(hass, entry.data["name"], entry.data["token_serial"], entry.data["serial_number"], entry.data["access_token"], entry.data["pin"], entry.data["app"])
+        hass.data.setdefault(DOMAIN, {})[entry.entry_id] = token.Token(hass, entry.data["name"], entry.data["api_ip_address"], input_config["token_serial"], input_config["serial_number"], input_config["access_token"], input_config["pin"], input_config["app"])
 
     # hass.data.setdefault(DOMAIN, {})[entry.entry_id] = token.Token(hass, entry.data["name"], entry.data["token_serial"], entry.data["serial_number"], entry.data["access_token"], entry.data["pin"], entry.data["app"]) if entry.entry_id not in hass.data.setdefault(DOMAIN, {}).keys() else False
 
