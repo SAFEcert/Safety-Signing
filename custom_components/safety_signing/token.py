2022fa9f931e842368acda2dc50f676942228a29
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-08 10:46:04 +0700

    -

--- custom_components/safety_signing/token.py
+++ custom_components/safety_signing/token.py
@@ -133,7 +133,12 @@ class Crons:
         }
         requestURL = API_URL + "/autoSign"
         # future = self._loop.run_in_executor(None, requests.post, requestURL, data=json.dumps(requestBody), headers=requestHeaders)
-        response = await self.token._hass.async_add_executor_job(lambda: requests.post(requestURL, data=json.dumps(requestBody), headers=requestHeaders))
+        try:
+            response = await self.token._hass.async_add_executor_job(lambda: requests.post(requestURL, data=json.dumps(requestBody), headers=requestHeaders))
+        except:
+            """"""
+            self._enable = "off"
+            response = False
         # response = asyncio.run_coroutine_threadsafe(
         #     requests.post(requestURL, data=json.dumps(requestBody), headers=requestHeaders), 1
         # ).result()
