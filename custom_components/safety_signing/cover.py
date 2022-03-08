9054f0d845309c3994bf994712af83239c2e1f29
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-08 11:24:31 +0700

    -

--- custom_components/safety_signing/cover.py
+++ custom_components/safety_signing/cover.py
@@ -12,8 +12,6 @@ from homeassistant.components.cover import (
     SUPPORT_SET_POSITION,
     CoverEntity,
 )
-from homeassistant.components.button import ButtonEntity
-from homeassistant.components.light import LightEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -38,18 +36,69 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 # This entire class could be written to extend a base class to ensure common attributes
 # are kept identical/in sync. It's broken apart here between the Cover and Sensors to
 # be explicit about what is returned, and the comments outline where the overlap is.
-class HelloWorldCover(LightEntity):
+class HelloWorldCover(CoverEntity):
     """Representation of a dummy Cover."""
+
+    # Our dummy class is PUSH, so we tell HA that it should not be polled
+    should_poll = False
+    # The supported features of a cover are done using a bitmask. Using the constants
+    # imported above, we can tell HA the features that are supported by this entity.
+    # If the supported features were dynamic (ie: different depending on the external
+    # device it connected to), then this should be function with an @property decorator.
+    supported_features = SUPPORT_SET_POSITION | SUPPORT_OPEN | SUPPORT_CLOSE
 
     def __init__(self, hass, cron) -> None:
         """Initialize the sensor."""
         # Usual setup is done here. Callbacks are added in async_added_to_hass.
         self._hass = hass
         self._cron = cron
-        self._attr_unique_id = f"{self._cron.cron_id}_button"
-        self._attr_name = f"{self._cron.name}_button"
-        self.is_light_on = False
 
+        # A unique_id for this entity with in this domain. This means for example if you
+        # have a sensor on this cover, you must ensure the value returned is unique,
+        # which is done here by appending "_cover". For more information, see:
+        # https://developers.home-assistant.io/docs/entity_registry_index/#unique-id-requirements
+        # Note: This is NOT used to generate the user visible Entity ID used in automations.
+        self._attr_unique_id = f"{self._cron.cron_id}_trigger"
+
+        # This is the name for this *entity*, the "name" attribute from "device_info"
+        # is used as the device name for device screens in the UI. This name is used on
+        # entity screens, and used to build the Entity ID that's used is automations etc.
+        self._attr_name = f"{self._cron.name}_trigger"
+
+    async def async_added_to_hass(self) -> None:
+        """Run when this Entity has been added to HA."""
+        # Importantly for a push integration, the module that will be getting updates
+        # needs to notify HA of changes. The dummy device has a registercallback
+        # method, so to this we add the 'self.async_write_ha_state' method, to be
+        # called where ever there are changes.
+        # The call back registration is done once this entity is registered with HA
+        # (rather than in the __init__)
+        self._cron.register_callback(self.async_write_ha_state)
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Entity being removed from hass."""
+        # The opposite of async_added_to_hass. Remove any registered call backs here.
+        self._cron.remove_callback(self.async_write_ha_state)
+
+    # Information about the devices that is partially visible in the UI.
+    # The most critical thing here is to give this entity a name so it is displayed
+    # as a "device" in the HA UI. This name is used on the Devices overview table,
+    # and the initial screen when the device is added (rather than the entity name
+    # property below). You can then associate other Entities (eg: a battery
+    # sensor) with this device, so it shows more like a unified element in the UI.
+    # For example, an associated battery sensor will be displayed in the right most
+    # column in the Configuration > Devices view for a device.
+    # To associate an entity with this device, the device_info must also return an
+    # identical "identifiers" attribute, but not return a name attribute.
+    # See the sensors.py file for the corresponding example setup.
+    # Additional meta data can also be returned here, including sw_version (displayed
+    # as Firmware), model and manufacturer (displayed as <model> by <manufacturer>)
+    # shown on the device info screen. The Manufacturer and model also have their
+    # respective columns on the Devices overview table. Note: Many of these must be
+    # set when the device is first added, and they are not always automatically
+    # refreshed by HA from it's internal cache.
+    # For more information see:
+    # https://developers.home-assistant.io/docs/device_registry_index/#device-properties
     @property
     def device_info(self) -> DeviceInfo:
         """Information about this entity/device."""
@@ -63,33 +112,59 @@ class HelloWorldCover(LightEntity):
         }
 
     @property
-    def available(self) -> bool:
-        """Return True if cron and token is available."""
-        return self._cron.online and self._cron.token.online
-
-    async def async_added_to_hass(self) -> None:
-        """Run when this Entity has been added to HA."""
-        self._cron.register_callback(self.async_write_ha_state)
-
-    async def async_will_remove_from_hass(self) -> None:
-        """Entity being removed from hass."""
-        self._cron.remove_callback(self.async_write_ha_state)
-    
-    @property
-    def is_on(self) -> bool:
-        return self.is_light_on
-
-    @property
     def icon(self) -> str:
         """Icon of the entity."""
         return "mdi:skip-next-circle"
 
-    async def async_turn_on(self, **kwargs):
-        """Turn device on."""
-        self.is_light_on = True
-        if self._cron.is_enable == "on":
-            await self._cron.running_cron()
+    # This property is important to let HA know if this entity is online or not.
+    # If an entity is offline (return False), the UI will refelect this.
+    @property
+    def available(self) -> bool:
+        """Return True if cron and token is available."""
+        return self._cron.online and self._cron.token.online
 
-    async def async_turn_off(self, **kwargs):
-        self.is_light_on = False
-        """Do nothing"""
+    # The following properties are how HA knows the current state of the device.
+    # These must return a value from memory, not make a live query to the device/token
+    # etc when called (hence they are properties). For a push based integration,
+    # HA is notified of changes via the async_write_ha_state call. See the __init__
+    # method for hos this is implemented in this example.
+    # The properties that are expected for a cover are based on the supported_features
+    # property of the object. In the case of a cover, see the following for more
+    # details: https://developers.home-assistant.io/docs/core/entity/cover/
+    @property
+    def current_cover_position(self):
+        """Return the current position of the cover."""
+        return self._cron.position
+
+    @property
+    def is_closed(self) -> bool:
+        """Return if the cover is closed, same as position 0."""
+        return self._cron.position == 0
+
+    @property
+    def is_closing(self) -> bool:
+        """Return if the cover is closing or not."""
+        return self._cron.moving < 0
+
+    @property
+    def is_opening(self) -> bool:
+        """Return if the cover is opening or not."""
+        return self._cron.moving > 0
+
+    # These methods allow HA to tell the actual device what to do. In this case, move
+    # the cover to the desired position, or open and close it all the way.
+    async def async_open_cover(self, **kwargs: Any) -> None:
+        """Open the cover."""
+        await self._cron.running_cron()
+        await self._cron.set_position(100)
+
+    async def async_close_cover(self, **kwargs: Any) -> None:
+        """Close the cover."""
+        
+        await self._cron.running_cron()
+        await self._cron.set_position(0)
+
+    async def async_set_cover_position(self, **kwargs: Any) -> None:
+        """Close the cover."""
+        await self._cron.running_cron()
+        await self._cron.set_position(kwargs[ATTR_POSITION])
