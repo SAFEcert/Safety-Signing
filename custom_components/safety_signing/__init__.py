e71fffeca45924bd82e4e346eb874ccff311233e
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-09 11:23:55 +0700

    Validate token & cert serial

--- custom_components/safety_signing/__init__.py
+++ custom_components/safety_signing/__init__.py
@@ -32,7 +32,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         return True
     
     token = Token(hass, entry.data["name"], entry.data["api_ip_address"], input_config["token_serial"], input_config["serial_number"], input_config["access_token"], input_config["pin"], input_config["app"])
-    token.check_serial_exists()
+    await token.check_serial_exists()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = token
     _LOGGER.info("Token ok")
 
